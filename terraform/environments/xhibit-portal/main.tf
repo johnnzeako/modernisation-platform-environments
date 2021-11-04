2030e4cf6e98f219b6fff991e6c406047b455d30
zuriguardiola <zuri.guardiola@digital.justice.gov.uk>
2021-11-04 08:52:22 +0000

    Add account number parameter

--- terraform/environments/xhibit-portal/main.tf
+++ terraform/environments/xhibit-portal/main.tf
@@ -1,9 +1,12 @@
+data "aws_caller_identity" "current" {}
+
 module "vm-import" {
 
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-vm-import"
+  source = "/Users/zuri/gitWork/mod-platform/modernisation-platform-terraform-aws-vm-import"
 
   bucket_prefix    = local.application_data.accounts[local.environment].bucket_prefix
   tags             = local.tags
   application_name = local.application_name
+  account_number   = data.aws_caller_identity.current.account_id
 
 }
